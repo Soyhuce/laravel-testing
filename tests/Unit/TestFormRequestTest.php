f2224d55ce878c4e2268d45e09f997020fa0ccfb
Bastien Philippe <bastien.philippe@soyhuce.fr>
2023-03-22 09:42:34 +0100

    Fix tests for Laravel 9

--- tests/Unit/TestFormRequestTest.php
+++ tests/Unit/TestFormRequestTest.php
@@ -86,7 +86,7 @@ class TestFormRequestTest extends TestCase
             ])
             ->assertFails([
                 'name' => 'The name field is required.',
-                'email' => 'The email field must be a valid email address.',
+                'email' => 'must be a valid email address.',
             ]);
     }
 
@@ -103,8 +103,8 @@ class TestFormRequestTest extends TestCase
             ])
             ->assertFails([
                 'email' => [
-                    'The email field must be a string.',
-                    'The email field must be a valid email address.',
+                    'must be a string.',
+                    'must be a valid email address.',
                 ],
             ]);
     }
